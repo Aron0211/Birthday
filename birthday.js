407ef305edcd7785f75e799c5ab19e4700ba6a44
papparon0902@gmail.com <papparon0902@gmail.com>
2026-01-16 23:16:49 +0100

    készen áll minden + reszponzív

---
+++ birthday.js
@@ -0,0 +1,7 @@
+function megjelenitSzoveget() {
+    // Kiválasztjuk a div-et
+    var doboz = document.getElementById("info-box");
+    
+    // Megjelenítjük
+    doboz.style.display = "block";
+}
